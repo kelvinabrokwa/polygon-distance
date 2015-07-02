510f0f6d1d7e9ce24ad7419b5734f8586319c084
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-07-02 02:20:44 -0400

    docs and fix

--- polygon-distance.js
+++ polygon-distance.js
@@ -1,19 +1,26 @@
 var point = require('turf-point'),
     distance = require('turf-distance');
 
+/**
+ * Find the shortest distance between two polygons
+ *
+ * @param {Object} poly1 geojson Polygon
+ * @param {Object} poly2 geojson Polygon
+ * @return {Number} shortest distance
+ */
 function polygonDistance(poly1, poly2) {
-  var distance = Infinity;
-  for (var i in poly1.coordinates) {
-    for (var j in poly2.coordinate) {
-      var pt1 = point(poly1.coordinates[i]),
-          pt2 = point(poly2.coordinates[j]),
+  var d = Infinity;
+  for (var i in poly1.coordinates[0]) {
+    for (var j in poly2.coordinates[0]) {
+      var pt1 = point(poly1.coordinates[0][i]),
+          pt2 = point(poly2.coordinates[0][j]),
           dist = distance(pt1, pt2);
-      if (dist < distance) {
-        distance = dist;
+      if (dist < d) {
+        d = dist;
       }
     }
   }
-  return distance;
+  return d;
 }
 
 module.exports = polygonDistance;
